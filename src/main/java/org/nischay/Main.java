c83302d8b52de4e3c58c6729ee1cab3cdfc1517b
Nischay J <nischay193@gmail.com>
2023-03-27 22:57:08 +0530

    feature(poke_sqlite): Setup SQLite

--- src/main/java/org/nischay/Main.java
+++ src/main/java/org/nischay/Main.java
@@ -1,11 +1,30 @@
 package org.nischay;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);
+
+            String createTableQuery =
+                    "CREATE TABLE nischay_test (\n" +
+                    "\tid INTEGER PRIMARY KEY,\n" +
+                    "\tfirst_name TEXT NOT NULL,\n" +
+                    "\tlast_name TEXT NOT NULL\n" +
+                    ")";
+            statement.executeUpdate(createTableQuery);
+            statement.close();
+            connection.close();
+            System.out.println("Table has been created successfully!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static int sq(int n) {
-        return n * n;
-    }
 }
