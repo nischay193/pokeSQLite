0642bb38e6d86a21fab329f33a63a6d91fb487f3
Nischay J <nischay193@gmail.com>
2023-03-29 23:06:44 +0530

    feature(poke_sqlite): API constructors, add jersey to deps

--- src/main/java/org/nischay/integration/api/Api.java
+++ src/main/java/org/nischay/integration/api/Api.java
@@ -1,5 +1,25 @@
 package org.nischay.integration.api;
 
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+
 public class Api {
+
+    private static final String DEFAULT_BASE_URL = "https://pokeapi.co/api/v2/";
+    private final String baseUrl;
+    private final Client httpClient;
+
+    public Api(String baseUrl) {
+        this.baseUrl = baseUrl;
+        this.httpClient = createHttpClient();
+    }
+    public Api() {
+        this(DEFAULT_BASE_URL);
+    }
+
+    private Client createHttpClient() {
+        return ClientBuilder.newClient();
+    }
+
 
 }
